ba45c3f82082f8458eb7c3ba20325bdfbd56dca5
Vincent Appel <cxt155@porus01.sund.root.ku.dk>
2014-12-16 09:57:35 +0100

    Updated to plink19 in subscripts

--- 1_scripted_QC/aimscript.sh
+++ 1_scripted_QC/aimscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # prepare plink files on aim snps only for pca analysis
-plink --noweb --bfile $1  --allow-no-sex --extract aimSNPs.txt --recodeA --out output/aimexome
+plink19 --bfile $1  --allow-no-sex --extract aimSNPs.txt --recodeA --out output/aimexome
 
 #removes phenotypic data (cols 2-6)
 cut -d " " -f1,7-  output/aimexome.raw  > output/exomeaimsnps.txt
--- 1_scripted_QC/mafauto.sh
+++ 1_scripted_QC/mafauto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-plink --noweb --bfile $1  --allow-no-sex --make-bed --maf 0.01 --out output/maf001 
-plink --noweb --bfile output/maf001 --write-snplist  --out output/snps_o_maf_001
-plink --noweb --bfile $1  --allow-no-sex --make-bed --exclude output/snps_o_maf_001.snplist --out output/maf_u001
-plink --noweb --bfile $1 --het  --allow-no-sex --out output/inbreed
-plink --noweb --bfile output/maf001 --het --allow-no-sex --out output/inbreed_maf001
-plink --noweb --bfile output/maf_u001 --het --allow-no-sex --out output/inbreed_maf_u001
+plink19 --bfile $1  --allow-no-sex --make-bed --maf 0.01 --out output/maf001 
+plink19 --bfile output/maf001 --write-snplist  --out output/snps_o_maf_001
+plink19 --bfile $1  --allow-no-sex --make-bed --exclude output/snps_o_maf_001.snplist --out output/maf_u001
+plink19 --bfile $1 --het  --allow-no-sex --out output/inbreed
+plink19 --bfile output/maf001 --het --allow-no-sex --out output/inbreed_maf001
+plink19 --bfile output/maf_u001 --het --allow-no-sex --out output/inbreed_maf_u001
