ca1ef453a7741b4b2fed6e09dd73c0c6df0ce221
jbkj <jbj@sund.ku.dk>
2020-09-23 13:42:31 +0200

    Update Rmiss.R

--- 1_scripted_QC/Rmiss.R
+++ 1_scripted_QC/Rmiss.R
@@ -2,11 +2,11 @@
 imiss <- read.table("output/MatrixMissing.imiss",h=T)
 lmiss <- read.table("output/MatrixMissing.lmiss",h=T)
 
-png("output/imiss.png")
+png("output/imiss.png",type = "cairo")
 plot(sort(imiss$F_MISS),ylab="Missingness",xlab="Sorted individuals",main="Missingness in individuals")
 dev.off()
 
-png("output/lmiss.png")
+png("output/lmiss.png",type = "cairo")
 plot(sort(lmiss$F_MISS),ylab="Missingness",xlab="Sorted SNPs",main="Missingness in alleles")
 dev.off()
 q(save="no")
