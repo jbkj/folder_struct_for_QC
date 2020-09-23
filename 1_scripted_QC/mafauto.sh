2cf00d3d33ee449e52e83f8cc66abbcc8b265929
jbkj <jbj@sund.ku.dk>
2020-09-23 15:03:00 +0200

    Update mafauto.sh

--- 1_scripted_QC/mafauto.sh
+++ 1_scripted_QC/mafauto.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-plink19 --bfile $1  --allow-no-sex --make-bed --maf 0.01 --out output/maf001 
-plink19 --bfile output/maf001 --write-snplist  --out output/snps_o_maf_001
-plink19 --bfile $1  --allow-no-sex --make-bed --exclude output/snps_o_maf_001.snplist --out output/maf_u001
 plink19 --bfile $1 --het  --allow-no-sex --out output/inbreed
-plink19 --bfile output/maf001 --het --allow-no-sex --out output/inbreed_maf001
-plink19 --bfile output/maf_u001 --het --allow-no-sex --out output/inbreed_maf_u001
+sed -e 's/[[:space:]]\+/ /g' output/inbreed.het > output/inbreed.het.sep
