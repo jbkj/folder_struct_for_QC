f1544fd69e5cf6c5d868393b1a40e42bf7fede24
jbkj <jbj@sund.ku.dk>
2021-04-06 12:40:29 +0200

    Update cohorts_fdt.sh

--- 1_scripted_QC/cohorts_fdt.sh
+++ 1_scripted_QC/cohorts_fdt.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-cut -f1 -d" " output/MatrixAllUpdate.fam > cohorttmp
+if [ -n "$2" ]
 
-grep -f cohorttmp $1 > particid_cohorttmp  
+then
 
-cut -f3 particid_cohorttmp -d";" |  sort -u > particid_unique_cohorttmp
+file=$(echo "output/"$2".fam")
+outfile=$(echo $2"_FDT_overview.txt")
+
+
+else
+
+
+file="output/MatrixAllUpdate.fam"
+outfile="cohort_FDT_overview.txt"
+
+
+
+fi
+echo $file > test
+echo $outfile >> test
+
+cut -f1 -d" " $file > cohorttmp
+
+grep -f cohorttmp $1 > particid_cohorttmp
+
+cut -f4,5 particid_cohorttmp -d";" |  sort -u > particid_unique_cohorttmp
 
 cut -f5 particid_cohorttmp -d";" |  cut -f1 -d"-"| sort -u > project_ids_cohorttmp
 
-paste <(echo "number of barcodes") <(wc -l cohorttmp| cut -f1 -d" ")> cohort_FDT_overview.txt
-paste <(echo "number of unique ids") <(wc -l particid_unique_cohorttmp | cut -f1 -d" ") >> cohort_FDT_overview.txt 
-paste <(echo "projectids in FDT file") <(cut -f3 particid_tmp -d";" |  cut -f1 -d"-"| sort -u)>> cohort_FDT_overview.txt
+paste <(echo "number of barcodes") <(wc -l cohorttmp| cut -f1 -d" ")> $outfile
+paste <(echo "number of unique ids") <(wc -l particid_unique_cohorttmp | cut -f1 -d" ") >> $outfile
+paste <(echo "projectids in FDT file") >> $outfile
 
-while read line; do  grep  ^$line particid_unique_cohorttmp > cohort_$line; done < project_ids_cohorttmp
+while read line; do  grep ";"$line$ particid_unique_cohorttmp > cohort_$line; done < project_ids_cohorttmp
 
-while read line; do paste <(echo "number of ids in cohort $line") <(wc -l cohort_$line| cut -f1 -d" ") >> cohort_FDT_overview.txt; done < project_ids_cohorttmp
+while read line; do paste <(echo "number of ids in cohort $line") <(wc -l cohort_$line| cut -f1 -d" ") >> $outfile; done < project_ids_cohorttmp
